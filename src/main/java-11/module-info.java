d50a71c53318ec5c453c37a83ffea9532be8f337
AO Industries, Inc <support@aoindustries.com>
2023-09-20 11:23:29 -0500

    JDK >= 11 required for building

---
+++ src/main/java-11/module-info.java
@@ -0,0 +1,25 @@
+/*
+ * pgp-keys-map - Additional PGP keys map for dependencies used by AO open-source projects.
+ * Copyright (C) 2023  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of pgp-keys-map.
+ *
+ * pgp-keys-map is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pgp-keys-map is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pgp-keys-map.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// Avoid cyclic dependency: Being in src/main/java-11 (instead of src/main/java) avoids some of the cycles
+module com.aoapps.pgp.keys.map {
+}
