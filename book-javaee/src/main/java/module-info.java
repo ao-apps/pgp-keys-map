7d2c3ed923b89ed3383b78778d3524630d0109b6
AO Industries, Inc <support@aoindustries.com>
2025-12-20 12:02:19 -0600

    Self-contained, offline builds of the jakartaee profile are now working
    This is not yet the actual upgrade to Jakarta EE 10.  This is just ensuring a
    consistent set of projects where the upgrade may be performed.
    
    1) Split books into book/ and book-javaee/ copies,
       with the latter being for compatibility during this transition only
    
    2) Updated dependency versions to only work within the jakartaee profile
    
    3) Updated cross-project paths within Jenkinsfile

---
+++ book-javaee/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * pgp-keys-map - Additional PGP keys map for dependencies used by AO open-source projects.
+ * Copyright (C) 2021, 2022, 2025  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of pgp-keys-map.
+ *
+ * pgp-keys-map is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pgp-keys-map is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pgp-keys-map.  If not, see <https://www.gnu.org/licenses/>.
+ */
+module com.aoapps.pgp.keys.map.book.javaee {
+  // Direct
+  requires com.aoapps.badges; // <groupId>com.aoapps</groupId><artifactId>ao-badges</artifactId>
+  requires com.aoapps.taglib; // <groupId>com.aoapps</groupId><artifactId>ao-taglib</artifactId>
+  requires com.semanticcms.changelog.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-changelog-taglib</artifactId>
+  requires com.semanticcms.core.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-taglib</artifactId>
+  requires com.semanticcms.section.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-taglib</artifactId>
+  requires taglibs.standard.spec; // <groupId>org.apache.taglibs</groupId><artifactId>taglibs-standard-spec</artifactId>
+}
